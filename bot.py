95c873f3616514977bf35ecd8514aaeacad93d6b
gordienkoalex <gordienkoalex@gmail.com>
2022-01-10 15:55:19 +0300

    make it better

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+
+
+TOKEN = "2110122103:AAERC4s9rn6sRUXFAhNv61Y7aCB5dazMlcQ"
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, "Приветули")
+
+@bot.message_handler(commands=['list'])
+def get_film_list(message):
+    bot.send_message(message.chat.id, "Список фильмов будет тут")
+
+
+bot.polling(none_stop=True)
