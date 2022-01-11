e8298f4087fcc2e914e11dc4f7a1008a371ff0b9
gordienkoalex <gordienkoalex@gmail.com>
2022-01-11 15:21:18 +0300

    112233343551

--- bot.py
+++ bot.py
@@ -8,10 +8,7 @@ TOKEN = "2110122103:AAERC4s9rn6sRUXFAhNv61Y7aCB5dazMlcQ"
 
 bot = telebot.TeleBot(TOKEN)
 
-markup = types.ReplyKeyboardMarkup(row_width=2)
-itembtn1 = types.KeyboardButton('Смотреть')
-itembtn2 = types.KeyboardButton('Отложить')
-markup.add(itembtn1, itembtn2)
+
 
 def check_user(message):
     return message.from_user.id in config.users
@@ -29,12 +26,15 @@ def get_film_list(message):
     if check_user(message):
         film = pg.get_film_data(pg.get_random_film())
         msg = bot.send_photo(message.chat.id,film['image'],f"{film['name']}({film['year']}) {film['duration']} мин. \n  {film['description']},filmId:{film['id']}",reply_markup=markup)
+        markup = types.ReplyKeyboardMarkup(row_width=2)
+        itembtn1 = types.KeyboardButton(f"Смотреть {film['id']}")
+        itembtn2 = types.KeyboardButton('Отложить')
+        markup.add(itembtn1, itembtn2)
         bot.register_next_step_handler(msg, delete_film_from_list)
     else:
         bot.send_message(message.chat.id, f"private data")
 
-def delete_film_from_list(message):
-    bot.send_message(message.chat.id, message.text)
+
 
 
 @bot.message_handler(commands=['list'])
@@ -48,8 +48,11 @@ def get_film_list(message):
 def parse_message(message):
     if check_user(message):
         try:
-            kpadapter.save_film_info_url(message.text)
-            bot.send_message(message.chat.id, 'Фильм добавлен в список. Для просомтра списка выполните команду /list')
+            if str(message.text).startswith('www'):
+                kpadapter.save_film_info_url(message.text)
+                bot.send_message(message.chat.id, 'Фильм добавлен в список. Для просомтра списка выполните команду /list')
+            elif str(message.text).startswith('Смотреть'):
+                bot.send_message(message.chat.id,message.text)
         except Exception as e:
             print(e)
     else:
